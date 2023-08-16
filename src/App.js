d19ee72f1b6c903769f032667ba662d388b3bc42
Golam Sarwar <sarwar@linkstaff.co.jp>
2023-08-16 11:19:57 +0900

    Update counter logic

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
         <h1>Number guessing game</h1>
         <p>
           We have selected a random number between 1 and 100. See if you can
-          guess it in 3 turns or fewer. We'll tell you if your guess was too
+          guess it in {numberOfTry} turns or fewer. We'll tell you if your guess was too
           high or too low.
         </p>
         <div className="guess">
